b59be7ef0b0f9694885f54fc2388b63000ed03e9
DaniM0613 <wwh.rj.92@gmail.com>
2024-10-14 13:34:20 -0600

    Trabajando con la accion de eliminar del carrito

--- src/reducers/cart-reducers.ts
+++ src/reducers/cart-reducers.ts
@@ -54,8 +54,10 @@ export const cartReducer = (
         }
     }
     if(action.type === 'remove-from-cart'){
+        const updatedCart = state.cart.filter(item => item.id !== action.payload.id)
         return {
-            ...state
+            ...state,
+            cart: updatedCart
         }
     }
     if(action.type === 'decrease-quantity'){
